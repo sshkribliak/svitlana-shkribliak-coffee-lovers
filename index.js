0db26fac8054d0d28c78d10ae4d29a646567aef0
Svitlana Shkribliak <134654266+sshkribliak@users.noreply.github.com>
2025-05-19 21:02:25 -0500

    added files and API requests

--- index.js
+++ index.js
@@ -4,3 +4,28 @@ fetch('https://api.github.com/users/sshkribliak/repos')
     .catch(error => {
         console.error("Error fetching repositories:", error);
     });
+
+fetch("https://api.sampleapis.com/coffee/hot")
+    .then(resp => resp.json())
+    .then(data => console.log(data[0].title));
+
+fetch("https://api.sampleapis.com/coffee/hot")
+    .then(resp => resp.json())
+    .then(coffee => {
+        let coffeeSection = document.getElementById('coffee-types');
+        for (let i = 0; i < coffee.length; i++) {
+            let coffeeType = document.createElement('div');
+            let coffeeTitle = document.createElement('h2');
+            coffeeTitle.innerText = coffee[i].title;
+            coffeeSection.appendChild(coffeeType);
+
+            let coffeeImage = document.createElement('img');
+            coffeeImage.src = coffee[i].image;
+            coffeeImage.style.maxWidth = "100%";
+            coffeeImage.style.height = "auto";
+            coffeeImage.id = 'coffee-image';
+            coffeeType.appendChild(coffeeTitle);
+            coffeeType.appendChild(coffeeImage);
+
+        }
+    })
